72358760f8058d37b12a940d445982d773731afb
Tomasz Rękawek <trekawek@gmail.com>
2014-01-16 19:47:08 +0100

    first version of SlingIterator

---
+++ src/main/java/com/cognifide/sling/query/Operation.java
@@ -0,0 +1,9 @@
+package com.cognifide.sling.query;
+
+import java.util.Iterator;
+
+import org.apache.sling.api.resource.Resource;
+
+public interface Operation {
+	Iterator<Resource> getResources(Resource resource);
+}
