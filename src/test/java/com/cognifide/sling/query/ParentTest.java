561cb8e5ed4efb51366c0683b73113f5800e5fb2
Tomasz Rękawek <trekawek@gmail.com>
2014-01-19 14:19:04 +0100

    added parent test

---
+++ src/test/java/com/cognifide/sling/query/ParentTest.java
@@ -0,0 +1,27 @@
+package com.cognifide.sling.query;
+
+import static com.cognifide.sling.query.api.SlingQuery.$;
+import static com.cognifide.sling.query.TestUtils.assertResourceListEquals;
+import static com.cognifide.sling.query.TestUtils.assertEmptyIterator;
+
+import org.apache.sling.api.resource.Resource;
+import org.junit.Test;
+
+import com.cognifide.sling.query.api.SlingQuery;
+
+public class ParentTest {
+
+	private Resource tree = TestUtils.getTree();
+
+	@Test
+	public void testParent() {
+		SlingQuery query = $(tree.getChild("application/configuration/labels")).parent();
+		assertResourceListEquals(query.iterator(), "configuration");
+	}
+
+	@Test
+	public void testRootParent() {
+		SlingQuery query = $(tree).parent();
+		assertEmptyIterator(query.iterator());
+	}
+}
