9f4c46935b5f9bb823ce10df2c27d9bc8195239a
Tomasz Rękawek <trekawek@gmail.com>
2014-03-04 10:56:05 +0100

    removed unused predicates

--- src/main/java/com/cognifide/sling/query/predicate/RejectingPredicate.java
+++ src/main/java/com/cognifide/sling/query/predicate/RejectingPredicate.java
@@ -4,19 +4,9 @@ import com.cognifide.sling.query.api.Predicate;
 
 public class RejectingPredicate<T> implements Predicate<T> {
 
-	private final Predicate<T> predicate;
-
-	public RejectingPredicate() {
-		this(new AcceptingPredicate<T>());
-	}
-
-	public RejectingPredicate(Predicate<T> predicate) {
-		this.predicate = predicate;
-	}
-
 	@Override
 	public boolean accepts(T value) {
-		return !predicate.accepts(value);
+		return false;
 	}
 
 }
