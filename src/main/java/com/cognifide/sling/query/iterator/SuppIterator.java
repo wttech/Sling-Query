2d958e80d39b277991649d8af2ec15858a8ceeef
Tomasz Rękawek <trekawek@gmail.com>
2014-03-06 19:02:03 +0100

    added docs

--- src/main/java/com/cognifide/sling/query/iterator/SuppIterator.java
+++ src/main/java/com/cognifide/sling/query/iterator/SuppIterator.java
@@ -7,8 +7,9 @@ import com.cognifide.sling.query.api.function.Option;
 import com.cognifide.sling.query.api.function.IteratorToIteratorFunction;
 
 /**
- * This class modifies the input iterator, removing all elements that are mapped to an empty value by the
- * function.
+ * This iterator returns all elements of the input list which are mapped to non-empty values by the input
+ * function. Name is inspired by the <a href="http://en.wikipedia.org/wiki/Support_(mathematics)">support of
+ * the function</a>.
  */
 public class SuppIterator<T> extends AbstractIterator<Option<T>> {
 
@@ -25,6 +26,12 @@ public class SuppIterator<T> extends AbstractIterator<Option<T>> {
 		this.output = function.apply(new ArgumentResettingIterator<T>(input.iterator()));
 	}
 
+	/**
+	 * The idea behind this method is that index of each element in the input iterator is passed to the
+	 * function. Elements returned by the output iterator contains the same index, which can be used to assign
+	 * input to output elements. We check which indices are present in the output iterator and return only
+	 * related input elements.
+	 */
 	@Override
 	protected Option<T> getElement() {
 		if (outputElement != null) {
